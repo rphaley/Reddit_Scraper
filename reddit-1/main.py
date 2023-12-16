6332b1a14e5c3b6cab0dd9a3b0bf8e8a799d174d
rphaley <rhaley@depaul.edu>
2023-12-15 22:05:19 -0600

    update with reddit auth

--- reddit-1/main.py
+++ reddit-1/main.py
@@ -3,26 +3,26 @@ debug = 0
 
 #TODO
 #Import modules
-import configparser, datetime, importlib, json, logging, openai, os, queue, random, re, requests, sys, threading, time, traceback
+import configparser, datetime, importlib, json, logging, openai, os, praw, queue, random, re, requests, sys, threading, time, traceback
 from datetime import datetime
 #Import custom modules
 from sendEmail import sendEmail
 from processDB import cleanJSON, readJSON, writeJSON
 from response import generateResponse
 #Import Google Cloud modules
-try:
-    if os.environ["GCP_Hosted"]:
+# try:
+#     if os.environ["GCP_Hosted"]:
         
-        if debug == 1: print(f'[+] GCP found, installing dependencies')
-        import google.cloud.logging
-        from google.cloud import storage
+#         if debug == 1: print(f'[+] GCP found, installing dependencies')
+#         import google.cloud.logging
+#         from google.cloud import storage
 
-        #Setup logging
-        client = google.cloud.logging.Client()
-        client.get_default_handler()
-        client.setup_logging()
-except Exception as e:
-    print(f'[-] Error importing GCP modules in main.py: {e}')
+#         #Setup logging
+#         client = google.cloud.logging.Client()
+#         client.get_default_handler()
+#         client.setup_logging()
+# except Exception as e:
+#     print(f'[-] Error importing GCP modules in main.py: {e}')
 
 
 #Print environment variables
@@ -33,60 +33,85 @@ except Exception as e:
 
 def check(site, config, data, db_queue, debug):
     #Get setting from config file
-    blstFlair = [item.strip() for item in config.get('settings', 'FlairExclusions').split(',')]
-    blstTitle = [item.strip() for item in config.get('settings', 'TitleExclusions').split(',')]
-    blstPost = [item.strip() for item in config.get('settings', 'PostExclusions').split(',')]
-    blstInclusions = [item.strip() for item in config.get('settings', 'Inclusions').split(',')]
-    blstFeatInclusions = [item.strip() for item in config.get('settings', 'FeatureInclusions').split(',')]
-    blstLocInclusions = [item.strip() for item in config.get('settings', 'LocationInclusions').split(',')]
-    winVer = [item.strip() for item in config.get('settings', 'winVer').split(',')]
+    blstFlair = [item.lstrip() for item in config.get('settings', 'FlairExclusions').split(',')]
+    blstTitle = [item.lstrip() for item in config.get('settings', 'TitleExclusions').split(',')]
+    blstPost = [item.lstrip() for item in config.get('settings', 'PostExclusions').split(',')]
+    blstInclusions = [item.lstrip() for item in config.get('settings', 'Inclusions').split(',')]
+    blstFeatInclusions = [item.lstrip() for item in config.get('settings', 'FeatureInclusions').split(',')]
+    blstLocInclusions = [item.lstrip() for item in config.get('settings', 'LocationInclusions').split(',')]
     blstTime = config.getint('settings', 'ScanTimeframe') * 3600
+    app_id = config.get('REDDITSettings', 'app_id')
+    secret = config.get('REDDITSettings', 'secret')
 
 
-    CN = re.findall('r\/\w{2}',site)
-    CN = CN[0][2:]
-    subredditName = re.findall('https\:\/\/www\.reddit\.com\/r\/(\w+)\/.*', site)
+    subredditName = site
+
+    # Get calling IP address
+    if debug == 1: 
+        try:
+            r = requests.get('http://ipinfo.io/ip')
+            print(r.text)
+        except Exception as e:
+            print(f'[-] Error getting public IP: {e}')
+
+    # Authenticate with Reddit
+    reddit = praw.Reddit(
+        client_id=app_id,
+        client_secret=secret,
+        user_agent="script:v1.0.0",
+    )
 
     #GET JSON FROM SUBREDDIT
     try:
-        response = requests.get(site, verify=True, timeout=5,headers = {f'User-agent': 'Mozilla/5.0 (Windows NT {random.choice(winVer)}; Win64; x64;)'})
+        #response = requests.get(site, verify=True, timeout=5,headers = {f'User-agent': random.choice(user_agents)})
+        posts = reddit.subreddit(site).new(limit=75)
     except Exception as e:
         print(f"[-] Client Error retrieving web data from {subredditName} Reason:{e}")
         logging.warning(f"[-] HTTP Client Error retrieving web data from {subredditName} Reason:{e}")
     
-    #Check for HTTP Errors
-    try:
-        parsed_json = json.loads(response.content)
-        if response.status_code != 200:
-            if debug == 1: print(f"[-] Server Error retrieving web data from {subredditName}")
-            if b'error' in parsed_json:
-                print(f"[-] Reddit error: Reason: '{parsed_json['reason']}' Message: '{parsed_json['message']}' from {subredditName}")
-                return
+    # #Check for HTTP Errors
+    # try:
+    #     if debug == 1: print(f'[+] Response status code: {response.status_code}')
+    #     if debug == 1: print(f'[+] Response content: {response.content[:500]}')
+    #     try:
+    #         parsed_json = json.loads(response.content)
+    #     except ValueError as e:
+    #         print(f'[-] Error parsing JSON: {e}')
+    #         return
+
+    #     if response.status_code != 200:
+    #         if debug == 1: print(f"[-] Server Error retrieving web data from {subredditName}")
+    #         if b'error' in parsed_json:
+    #             print(f"[-] Reddit error: Reason: '{parsed_json['reason']}' Message: '{parsed_json['message']}' from {subredditName}")
+    #             return
             
-    except Exception as e:
-        print(f"[-] Error retrieving web data from {subredditName} Reason:{e}")
-        logging.warning(f"[-] Error retrieving web data from {subredditName} Reason:{e}")
-        return
+            
+    # except Exception as e:
+    #     print(f"[-] Error retrieving web data from {subredditName} Reason:{e}")
+    #     logging.warning(f"[-] Error retrieving web data from {subredditName} Reason:{e}")
+    #     return
     
     #Iterate through posts
-    for post in range(0,75):
+    for post in posts:
+        #post = vars(post)
         #Check for stickied threads
         bad = 0
         #Check is thread is stickied, ignore
-        if parsed_json['data']['children'][post]['data']['stickied'] == True:
+        if post.stickied == True:
             if debug == 1: print(f'Stickied Thread in {subredditName}, ignoring')
             continue
         try:
             #Parse specific output
-            flair = parsed_json['data']['children'][post]['data']['link_flair_text']
-            title = parsed_json['data']['children'][post]['data']['title']
-            postBody = parsed_json['data']['children'][post]['data']['selftext']
-            author = parsed_json['data']['children'][post]['data']['author']
-            id = parsed_json['data']['children'][post]['data']['id']
-            createdTime = parsed_json['data']['children'][post]['data']['created_utc']
-            url = parsed_json['data']['children'][post]['data']['url']
+            flair = post.link_flair_text
+            title = post.title
+            postBody = post.selftext
+            author = post.author.name if post.author else '[deleted]'
+            id = post.id
+            createdTime = post.created_utc
+            url = post.url
             if debug == 1: print(f'Flair:{flair}, Title:{title}, URL:{url}') 
         except Exception as e:
+            print(vars(post))
             # Get the current exception information
             exc_type, exc_obj, tb = sys.exc_info()
             # Get the line number where the exception occurred
@@ -102,15 +127,15 @@ def check(site, config, data, db_queue, debug):
 
         #Check if post is within timeframe
         if int(time.time()) - createdTime > blstTime:
-            if debug == 1: print(f'[BAD][{subredditName[0]}][{id}] Post is too old.  Ignoring.')
+            if debug == 1: print(f'[BAD][{subredditName}][{id}] Post is too old.  Ignoring.')
             continue
 
         #Check id against database
         if id in data:
-            if debug == 1: print(f'[BAD][{subredditName[0]}][{id}] ID:{id} already in database {data}.')
+            if debug == 1: print(f'[BAD][{subredditName}][{id}] ID:{id} already in database {data}.')
             continue
         else:
-            if debug == 1: print(f'[GOOD][{subredditName[0]}][{id}] ID:{id} not in database {data}. Continuing...')
+            if debug == 1: print(f'[GOOD][{subredditName}][{id}] ID:{id} not in database {data}. Continuing...')
 
         postBad = 0
         #Check exclusion in post body
@@ -118,9 +143,9 @@ def check(site, config, data, db_queue, debug):
             postBody_tmp = postBody.upper()
             for j in blstPost:
                 j = j.upper()
-                if debug == 1: print(f'[PostEx Check][{subredditName[0]}][{id}] CurrentCheck:{j}, Post:{postBody_tmp}')
+                if debug == 1: print(f'[PostEx Check][{subredditName}][{id}] CurrentCheck:{j}, Post:{postBody_tmp}')
                 if j in postBody_tmp:
-                    if debug == 1: print(f'[BAD][{subredditName[0]}][{id}] Body keyword exclusion "{j}" on:{postBody_tmp}')
+                    if debug == 1: print(f'[BAD][{subredditName}][{id}] Body keyword exclusion "{j}" on:{postBody_tmp}')
                     postBad = postBad + 1
                     continue
             if postBad > 1: continue
@@ -133,7 +158,7 @@ def check(site, config, data, db_queue, debug):
             try:
                 age = int(age)
                 if int(age) < 25 or int(age) > 40:
-                    if debug == 1: print(f'[BAD][{subredditName[0]}][{id}] Incorrect age:{age} on:{title}')
+                    if debug == 1: print(f'[BAD][{subredditName}][{id}] Incorrect age:{age} on:{title}')
                     bad = 1
                     continue
             except ValueError:
@@ -141,9 +166,9 @@ def check(site, config, data, db_queue, debug):
 
             for j in blstTitle:
                 j = j.upper()
-                if debug == 1: print(f'[TitleEx Check][{subredditName[0]}][{id}] CurrentCheck:{j}, Title:{title}')
+                if debug == 1: print(f'[TitleEx Check][{subredditName}][{id}] CurrentCheck:{j}, Title:{title}')
                 if j in title:
-                    if debug == 1: print(f'[BAD][{subredditName[0]}][{id}] Title keyword exclusion "{j}" on:{title}')
+                    if debug == 1: print(f'[BAD][{subredditName}][{id}] Title keyword exclusion "{j}" on:{title}')
                     bad = 1
                     continue
             if bad == 1: continue
@@ -153,9 +178,9 @@ def check(site, config, data, db_queue, debug):
             flair = flair.upper()
             for k in blstFlair:
                 k = k.upper()
-                if debug == 1: print(f'[FlairEx Check][{subredditName[0]}][{id}] CurrentCheck:{k}, Title:{flair}')
+                if debug == 1: print(f'[FlairEx Check][{subredditName}][{id}] CurrentCheck:{k}, Title:{flair}')
                 if k in flair:
-                    if debug == 1: print(f'[BAD][{subredditName[0]}][{id}] Flair keyword exclusion "{k}" on:{flair}')
+                    if debug == 1: print(f'[BAD][{subredditName}][{id}] Flair keyword exclusion "{k}" on:{flair}')
                     bad = 1
                     continue
             if bad == 1: continue
@@ -171,16 +196,18 @@ def check(site, config, data, db_queue, debug):
             locGood = 0
             for loc in blstLocInclusions:
                 loc = loc.upper()
-                if debug == 1: print(f'[LocationInclusionCheck][{subredditName[0]}][{id}] CurrentCheck:{loc}, Post:{postBody_tmp} Title:{title}')
+                if debug == 1: print(f'[LocationInclusionCheck][{subredditName}][{id}] CurrentCheck:{loc}, Post:{postBody_tmp} Title:{title}')
                 if loc in postBody_tmp or loc in title:
-                    if debug == 1: print(f'[LocationInclusionCheck][GOOD][{subredditName[0]}][{id}] keyword "{loc}" found on:{postBody_tmp.rstrip()} or {title}')
+                    if debug == 1: print(f'[LocationInclusionCheck][GOOD][{subredditName}][{id}] keyword "{loc}" found on:{postBody_tmp.rstrip()} or {title}')
                     if loc in postBody_tmp:
-                        keywork_match_lst.append(''.join(c for c in loc if c.isalnum()))
-                        if debug == 1: print(f'[LocationInclusionCheck][GOOD][{subredditName[0]}][{id}] Post keyword "{loc}" found on:{postBody_tmp}')
+                        #keywork_match_lst.append(''.join(c for c in loc if c.isalnum()))
+                        keywork_match_lst.append(loc)
+                        if debug == 1: print(f'[LocationInclusionCheck][GOOD][{subredditName}][{id}] Post keyword "{loc}" found on:{postBody_tmp}')
                         locGood = locGood + 1
                     elif loc in title:
-                        keywork_match_lst.append(''.join(c for c in loc if c.isalnum()))
-                        if debug == 1: print(f'[LocationInclusionCheck][GOOD][{subredditName[0]}][{id}] Title keyword "{loc}" found on:{title}')
+                        #keywork_match_lst.append(''.join(c for c in loc if c.isalnum()))
+                        keywork_match_lst.append(loc)
+                        if debug == 1: print(f'[LocationInclusionCheck][GOOD][{subredditName}][{id}] Title keyword "{loc}" found on:{title}')
                         locGood = locGood + 1
             if locGood == 0: continue
 
@@ -189,31 +216,35 @@ def check(site, config, data, db_queue, debug):
             good = 0
             for feat in blstFeatInclusions:
                 feat = feat.upper()
-                if debug == 1: print(f'[FeatureInclusionCheck][{subredditName[0]}][{id}] CurrentCheck:{feat}, Post:{postBody_tmp} Title:{title}')
+                if debug == 1: print(f'[FeatureInclusionCheck][{subredditName}][{id}] CurrentCheck:{feat}, Post:{postBody_tmp} Title:{title}')
                 if feat in postBody_tmp or feat in title:
-                    if debug == 1: print(f'[FeatureInclusionCheck][GOOD][{subredditName[0]}][{id}] keyword "{feat}" found on:{postBody_tmp.rstrip()} or {title}')
+                    if debug == 1: print(f'[FeatureInclusionCheck][GOOD][{subredditName}][{id}] keyword "{feat}" found on:{postBody_tmp.rstrip()} or {title}')
                     if feat in postBody_tmp:
-                        keywork_match_lst.append(''.join(c for c in feat if c.isalnum()))
-                        if debug == 1: print(f'[FeatureInclusionCheck][GOOD][{subredditName[0]}][{id}] Post keyword "{feat}" found on:{postBody_tmp}')
+                        #keywork_match_lst.append(''.join(c for c in feat if c.isalnum()))
+                        keywork_match_lst.append(feat)
+                        if debug == 1: print(f'[FeatureInclusionCheck][GOOD][{subredditName}][{id}] Post keyword "{feat}" found on:{postBody_tmp}')
                         good = good + 1
                     elif feat in title:
-                        keywork_match_lst.append(''.join(c for c in feat if c.isalnum()))
-                        if debug == 1: print(f'[FeatureInclusionCheck][GOOD][{subredditName[0]}][{id}] Title keyword "{feat}" found on:{title}')
+                        #keywork_match_lst.append(''.join(c for c in feat if c.isalnum()))
+                        keywork_match_lst.append(feat)
+                        if debug == 1: print(f'[FeatureInclusionCheck][GOOD][{subredditName}][{id}] Title keyword "{feat}" found on:{title}')
                         good = good + 1
 
 
             for j in blstInclusions:
                 j = j.upper()
-                if debug == 1: print(f'[InclusionCheck][{subredditName[0]}][{id}] CurrentCheck:{j}, Post:{postBody_tmp} Title:{title}')
+                if debug == 1: print(f'[InclusionCheck][{subredditName}][{id}] CurrentCheck:{j}, Post:{postBody_tmp} Title:{title}')
                 if j in postBody_tmp or j in title:
-                    if debug == 1: print(f'[InclusionCheck][GOOD][{subredditName[0]}][{id}] keyword "{j}" found on:{postBody_tmp.rstrip()} or {title}')
+                    if debug == 1: print(f'[InclusionCheck][GOOD][{subredditName}][{id}] keyword "{j}" found on:{postBody_tmp.rstrip()} or {title}')
                     if j in postBody_tmp:
-                        keywork_match_lst.append(''.join(c for c in j if c.isalnum()))
-                        if debug == 1: print(f'[InclusionCheck][GOOD][{subredditName[0]}][{id}] Post keyword "{j}" found on:{postBody_tmp}')
+                        #keywork_match_lst.append(''.join(c for c in j if c.isalnum()))
+                        keywork_match_lst.append(j)
+                        if debug == 1: print(f'[InclusionCheck][GOOD][{subredditName}][{id}] Post keyword "{j}" found on:{postBody_tmp}')
                         good = good + 1
                     elif j in title:
-                        keywork_match_lst.append(''.join(c for c in j if c.isalnum()))
-                        if debug == 1: print(f'[InclusionCheck][GOOD][{subredditName[0]}][{id}] Title keyword "{j}" found on:{title}')
+                        #keywork_match_lst.append(''.join(c for c in j if c.isalnum()))
+                        keywork_match_lst.append(j)
+                        if debug == 1: print(f'[InclusionCheck][GOOD][{subredditName}][{id}] Title keyword "{j}" found on:{title}')
                         good = good + 1
             if good < 2: continue
             if len(keywork_match_lst) < 2: continue
@@ -229,7 +260,7 @@ def check(site, config, data, db_queue, debug):
         try:
             #Send email
             print('='*50 + 'EMAIL' + '='*50)
-            sendEmail(config, debug, f'{title}',url,subredditName[0], postBody, author, postResponse, keywork_match_lst)
+            sendEmail(config, debug, f'{title}',url,subredditName, postBody, author, postResponse, keywork_match_lst)
         except Exception as e:
             print(f'[-] Error sending email: {e}')
             continue
@@ -238,10 +269,10 @@ def check(site, config, data, db_queue, debug):
         # data[id] = {"createdTime":createdTime,"keywordMatch":keywork_match_lst}
         # db_queue.put(data)
         human_time = str(datetime.fromtimestamp(createdTime).strftime('%Y-%m-%d_%H:%M:%S'))
-        db_queue.put({id:{"author":author,"createdTime":createdTime,"humanTime":human_time,"keywordMatch":keywork_match_lst,"subreddit":subredditName[0],"title":title,"url":url}})
+        db_queue.put({id:{"author":author,"createdTime":createdTime,"humanTime":human_time,"keywordMatch":keywork_match_lst,"subreddit":subredditName,"title":title,"url":url}})
 
 
-    print('='*50 + f'CHECK {post} COMPLETE ON {subredditName[0]}' + '='*50)
+    print('='*50 + f'CHECK {post} COMPLETE ON {subredditName}' + '='*50)
 
 
         
@@ -259,10 +290,11 @@ def Hosts(config, debug):
         return
 
     #Get list of uris from config file
-    sites = [item.strip() for item in config.get('settings', 'sites').split(',')]
+    subreddits = [item.strip() for item in config.get('settings', 'subreddits').split(',')]
+
     db_queue = queue.Queue()
-    for uri in sites:
-        if debug == 1: print("Trying {}...".format(uri))
+    for uri in subreddits:
+        if debug == 1: print(f'Trying "{uri}"')
         t = threading.Thread(target=check, args=(uri, config, data, db_queue, debug,)) #Create thread for each host
         #kill process if main thread ends
         t.daemon = True            
@@ -319,4 +351,4 @@ def main(request):
 
 
 #For local host testing
-#main('test')
+main('test')
